36d5976cd93e30d958044126fd2269dbd1a6f372
Dmitry Vasilevich <wotlk777@gmail.com>
2019-10-23 20:50:55 +0300

    initial

---
+++ Self.Drive.Web/Controllers/DocumentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Self.Drive.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class DocumentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("42");
+        }
+    }
+}
---
+++ Self.Drive.Web/Models/DocumentsDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Self.Drive.Web.Models
+{
+    public class DocumentsDatabaseSettings : IDocumentsDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string DocumentsCollectionName { get; set; }
+    }
+
+    public interface IDocumentsDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+        string DocumentsCollectionName { get; set; }
+    }
+}
